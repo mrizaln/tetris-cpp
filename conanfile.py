7549fbdf7ac10ca9c9559c71df2ad1450dc903f4
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-08-03 20:01:45 +0700

    Add spdlog window creation and logging initialization

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conan import ConanFile
 class Recipe(ConanFile):
     settings   = ["os", "compiler", "build_type", "arch"]
     generators = ["CMakeToolchain", "CMakeDeps"]
-    requires   = ["fmt/10.2.1"]
+    requires   = ["fmt/10.2.1", "sfml/2.6.1", "spdlog/1.14.1"]
 
     def layout(self):
         self.folders.generators = "conan"
--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,43 @@
+#include <SFML/Graphics.hpp>
 #include <fmt/core.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
 int main()
 {
+    spdlog::set_default_logger(spdlog::stderr_color_st("logger"));
+    spdlog::set_pattern("[%H:%M:%S %z] [%^-%L-%$] %v");
+    spdlog::set_level(spdlog::level::debug);
+
     fmt::println("Hello from 'tetris-cpp'!");
+
+    auto videoMode = sf::VideoMode{ 800, 600 };
+    auto window    = sf::RenderWindow{ videoMode, "Tetris" };
+
+    window.setVerticalSyncEnabled(true);
+    window.setKeyRepeatEnabled(true);
+    window.setActive(true);
+
+    while (window.isOpen()) {
+        for (sf::Event event{}; window.pollEvent(event);) {
+            using E = sf::Event;
+            using K = sf::Keyboard::Key;
+
+            switch (event.type) {
+            case E::Closed: window.close(); break;
+            case E::KeyPressed: {
+                if (event.key.code == K::Q) {
+                    window.close();
+                }
+            } break;
+            default: /* do nothing */ break;
+            }
+        }
+
+        window.clear(sf::Color::Magenta);
+
+        // draw here ...
+
+        window.display();
+    }
 }
